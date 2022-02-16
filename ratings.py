fa7b5db5d0e343a401220c32305e140f23876b1c
Cory Owen <cjowen14@outlook.com>
2022-02-16 10:28:54 -0700

    Added Rating Validator

--- ratings.py
+++ ratings.py
@@ -3,18 +3,32 @@
 
 # put your code here
 
+#function to ensure rating is between 1-5
+def user_input():
+    new_rat = input("What is the rating for the restaurant?\n")
+    if float(new_rat) <=5 and float(new_rat) >=1:
+        dictionary[new_rest] = new_rat
+    else:
+        print("Rating must be between 1-5.")
+        user_input()
+
+#import txt file
 dictionary = {}
 with open("scores.txt") as scores:
     for line in scores:
         (key, val) = line.rstrip("\n").split(":")
         dictionary[key] = val
 
+#get user input for restaurant name and rating
 new_rest = input("What is the name of the new restaurant?\n")
-new_rat = input("What is the rating for the restaurant?\n")
-dictionary[new_rest] = new_rat
+user_input()
+
+#create a list with the dictionary items
 list = dictionary.items()
 
+#sort the new list
 sorted_list = sorted(list)
 
+#loop through the list to print name and rating in alphabetical order by restaurant
 for i in sorted_list:
     print(i[0], "is rated at", i[1])
