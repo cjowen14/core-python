0a979497e476fb72ead3df01987232f1cd85b83a
Cory Owen <cjowen14@outlook.com>
2022-02-16 16:42:22 -0700

    Additional Functionality and Fixed

--- ratings.py
+++ ratings.py
@@ -15,22 +15,22 @@ def open_dictionary(file_input):
     main(file_input)
 
 #get new restaurant name        
-def rest_name():
+def rest_name(file_input):
     new_rest = input("What is the name of the new restaurant?\n")
-    rating_input(new_rest)
+    rating_input(new_rest, file_input)
 
 #get new restaurant rating and ensure its between 1-5
-def rating_input(new_rest):
+def rating_input(new_rest, file_input):
     new_rat = input("What is the rating for the restaurant?\n")
     if float(new_rat) <=5 and float(new_rat) >=1:
         dictionary[new_rest] = new_rat
-        see_ratings()
+        see_ratings(file_input)
     else:
         print("Rating must be between 1-5.")
-        rating_input(new_rest)
+        rating_input(new_rest, file_input)
 
 #see ratings of current restaurants
-def see_ratings():
+def see_ratings(file_input):
     #create a list with the dictionary items
     list = dictionary.items()
     #sort the new list
@@ -39,20 +39,20 @@ def see_ratings():
     for i in sorted_list:
         print(i[0], "is rated at", i[1])
     print("\n")
-    main()
+    main(file_input)
 
 #provide user with a random restaurant and let them change the rating
-def change_random():
+def change_random(file_input):
     list = dictionary.items()
     sorted_list = sorted(list)
     random_choice = random.choice(sorted_list)
     print("Your restaurant is", random_choice[0], "which has a current rating of", random_choice[1])
     new_rating = input("What would you like to change the rating to?\n")
     dictionary[random_choice[0]] = new_rating;
-    main()
+    main(file_input)
 
 #allow user to select a restaurant to change the rating
-def change_selected():
+def change_selected(file_input):
     list = dictionary.items()
     sorted_list = sorted(list)
     for i in sorted_list:
@@ -64,19 +64,19 @@ def change_selected():
         if x == user_choice:
             dictionary[x] = new_rating
     print("Rating for", user_choice, "has been updated!")
-    main()
+    main(file_input)
 
 #user input for what they would like to do
 def main(file_input):
     user_choice = int(input("What would you like to do? You are currently in " + file_input + " (Enter the number)\n 1. See all ratings\n 2. Add a new restaurant\n 3. Change Random Rating\n 4. Choose a restaurant to update a rating\n 5. Choose another txt file\n 6. Quit\n"))
     if user_choice == 1:
-        see_ratings()
+        see_ratings(file_input)
     elif user_choice == 2:
-        rest_name()
+        rest_name(file_input)
     elif user_choice == 3:
-        change_random()
+        change_random(file_input)
     elif user_choice == 4:
-        change_selected()
+        change_selected(file_input)
     elif user_choice == 5:
         file_choice()
     elif user_choice == 6:
